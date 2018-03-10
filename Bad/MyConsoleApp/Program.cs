3e9b15549583a29b2cc03f1bd700c79e637e2983
Ahmed Tarek Hasan <eng_ahmed.tarek@hotmail.com>
2018-03-10 09:22:02 +0200

    Add files via upload

---
+++ Bad/MyConsoleApp/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+
+            do
+            {
+                Console.WriteLine("Welcome to my console app");
+                Console.WriteLine("[1] Say Hello?");
+                Console.WriteLine("[2] Say Goodbye?");
+                Console.WriteLine("");
+                Console.Write("Please enter a valid choice: ");
+
+                input = Console.ReadLine();
+
+                if (input == "1" || input == "2")
+                {
+                    Console.Write("Please enter your name: ");
+                    string name = Console.ReadLine();
+
+                    if (input == "1")
+                    {
+                        Console.WriteLine("Hello " + name);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Goodbye " + name);
+                    }
+
+                    Console.WriteLine("");
+                    Console.Write("Press any key to exit... ");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.Clear();
+                }
+            }
+            while (input != "1" && input != "2");
+        }
+    }
+}
